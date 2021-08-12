f98083e4fd541a5182acaac06061821df0069dde
Jeannette <gimezlug@gmail.com>
2021-08-12 15:14:00 -0400

    feature:Se agrego descripcion con codigo en la lista conceptos ISLR
    #187zrgf

--- addons/l10n_ve_plusteam/models/conceptos_ISLR.py
+++ addons/l10n_ve_plusteam/models/conceptos_ISLR.py
@@ -6,9 +6,24 @@ from odoo import models, fields, api
 class ConceptosISLR(models.Model):
     _name = "conceptos.islr"
     _description = "Conceptos ISLR"
+    _rec_name = "complete_name_with_code"
 
-    code = fields.Char(string="Code", required=True)
+    # _sql_constraints = [('code_unique', 'unique(code)', 'Name should be unique')]
+
+    code = fields.Char(string="Code", required=True, index=True)
     name = fields.Char(string="Description", translate=True, required=True)
     description = fields.Char(string="Description Complete", translate=True)
     literal = fields.Char(string="Literal")
     numeral = fields.Integer(string="Numeral")
+
+    complete_name_with_code = fields.Char(
+        "Complete Name with Code",
+        compute="_compute_complete_name_with_code",
+        store=True
+    )
+
+    @api.depends("name", "code")
+    def _compute_complete_name_with_code(self):
+        for islr in self:
+            islr.complete_name_with_code = f'[{islr.code}] {islr.name}'
+
