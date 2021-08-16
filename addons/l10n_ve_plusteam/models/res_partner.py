b05d85716656e2d3c3f5fcebb913e1b39d322b2d
Anny Chacon <ac@plusteam.tech>
2021-08-16 07:59:53 -0400

    feat: 🌻 Add special taxpayer field to contact model
    Refs #65cefc

--- addons/l10n_ve_plusteam/models/res_partner.py
+++ addons/l10n_ve_plusteam/models/res_partner.py
@@ -16,6 +16,17 @@ class ResPartner(models.Model):
         default=True,
         help=_("It is used to filter the contributors for the sales book report")
     )
+    special_taxpayer = fields.Boolean(
+        string="Special Taxpayer",
+        default=False,
+        help=_("It is used to know if the person is a taxpayer or not and to apply withholding.")
+    )
+
+    @api.onchange("taxpayer")
+    def _onchange_taxpayer_field(self):
+        for record in self:
+            if record.taxpayer is False:
+                record.special_taxpayer = False
 
     @api.constrains('vat')
     def _check_rif_field(self):
