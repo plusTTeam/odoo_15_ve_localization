896bc97d7f4e8d61e5e5977eb912213470f25d91
Jeannette <gimelug@gmail.com>
2021-08-18 15:16:07 -0400

    feature: boton seniat

--- addons/l10n_ve_plusteam/models/res_partner.py
+++ addons/l10n_ve_plusteam/models/res_partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, api
+from odoo import fields, models, api, _
 from odoo.exceptions import ValidationError
 import re
 
@@ -16,4 +16,5 @@ class ResPartner(models.Model):
         for record in self:
             if re.match(r"^[V|E|J|P][0-9]{7,9}$", record.vat) is None:
                 raise ValidationError("Formato RIF/Cédula Invalido. Debe comenzar con una letra (V,J,E,P) seguido de 7 o 9 números")
+
 
