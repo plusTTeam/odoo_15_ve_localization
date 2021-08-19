a4bd847dc661709735fdb0c27a44447a14561327
Jeannette <gimezlug@gmail.com>
2021-08-19 19:04:49 -0400

    refactor: cambios segun review

--- addons/l10n_ve_plusteam/models/account_move.py
+++ addons/l10n_ve_plusteam/models/account_move.py
@@ -9,14 +9,14 @@ import re
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    number_control = fields.Char(string="Control Number")
+    control_number = fields.Char(string="Control Number")
     form_number = fields.Char(string="form number", help="Import form number C-80 o C-81", defaul="  ")
     file_number = fields.Char(string="file number", help="Import file number", defaul="  ")
     import_doc = fields.Boolean(string="Import")
 
-    @api.constrains('number_control')
+    @api.constrains('control_number')
     def _check_number_control(self):
         for record in self:
-            if re.match(r"^[0-9]{6,9}$", record.number_control) is None:
+            if re.match(r"^[0-9]{6,9}$", record.control_number) is None:
                 raise ValidationError("Formato Numero de control Invalido. Debe tener minimo 6 números")
 
