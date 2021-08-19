9c91e319823fe7340c1d0e6ce7dee0b8ee6a3892
Jeannette <gimezlug@gmail.com>
2021-08-19 18:37:51 -0400

    refactor: Arreglos segun review

--- addons/l10n_ve_plusteam/models/res_config_settings.py
+++ addons/l10n_ve_plusteam/models/res_config_settings.py
@@ -3,29 +3,30 @@
 from odoo import models, fields, api
 
 
-class Resconfigsettings(models.TransientModel):
+class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
+    ACCOUNT_MODEL = "account.account"
 
-    purchase_account_iva_id = fields.Many2one("account.account", string="VAT accounting account for suppliers")
-    sale_account_iva_id = fields.Many2one("account.account", string="VAT accounting account for customer")
-    purchase_account_islr_id = fields.Many2one("account.account", string="ISLR accounting account for suppliers")
-    sale_account_islr_id = fields.Many2one("account.account", string="ISLR accounting account for customer")
+    iva_account_purchase_id = fields.Many2one(ACCOUNT_MODEL, string="VAT accounting account for suppliers")
+    iva_account_sale_id = fields.Many2one(ACCOUNT_MODEL, string="VAT accounting account for customer")
+    islr_account_purchase_id = fields.Many2one(ACCOUNT_MODEL, string="ISLR accounting account for suppliers")
+    islr_account_sale_id = fields.Many2one(ACCOUNT_MODEL, string="ISLR accounting account for customer")
 
 
     def set_values(self):
-        super(Resconfigsettings, self).set_values()
+        super(ResConfigSettings, self).set_values()
         set_param = self.env['ir.config_parameter'].sudo().set_param
-        set_param('l10n_ve_plusteam.purchase_account_iva_id', int(self.purchase_account_iva_id.id))
-        set_param('l10n_ve_plusteam.sale_account_iva_id', int(self.sale_account_iva_id.id))
-        set_param('l10n_ve_plusteam.purchase_account_islr_id', int(self.purchase_account_islr_id.id))
-        set_param('l10n_ve_plusteam.sale_account_islr_id', int(self.sale_account_islr_id.id))
+        set_param('l10n_ve_plusteam.iva_account_purchase_id', int(self.iva_account_purchase_id.id))
+        set_param('l10n_ve_plusteam.iva_account_sale_id', int(self.iva_account_sale_id.id))
+        set_param('l10n_ve_plusteam.islr_account_purchase_id', int(self.islr_account_purchase_id.id))
+        set_param('l10n_ve_plusteam.islr_account_sale_id', int(self.islr_account_sale_id.id))
 
     @api.model
     def get_values(self):
-        res = super(Resconfigsettings, self).get_values()
+        res = super(ResConfigSettings, self).get_values()
         get_param = self.env['ir.config_parameter'].sudo().get_param
-        res['purchase_account_iva_id'] = int(get_param('l10n_ve_plusteam.purchase_account_iva_id'))
-        res['sale_account_iva_id'] = int(get_param('l10n_ve_plusteam.sale_account_iva_id'))
-        res['purchase_account_islr_id'] = int(get_param('l10n_ve_plusteam.purchase_account_islr_id'))
-        res['sale_account_islr_id'] = int(get_param('l10n_ve_plusteam.sale_account_islr_id'))
+        res['iva_account_purchase_id'] = int(get_param('l10n_ve_plusteam.iva_account_purchase_id'))
+        res['iva_account_sale_id'] = int(get_param('l10n_ve_plusteam.iva_account_sale_id'))
+        res['islr_account_purchase_id'] = int(get_param('l10n_ve_plusteam.islr_account_purchase_id'))
+        res['islr_account_sale_id'] = int(get_param('l10n_ve_plusteam.islr_account_sale_id'))
         return res
