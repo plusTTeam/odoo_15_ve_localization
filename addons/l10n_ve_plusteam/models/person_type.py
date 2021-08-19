91e1b5a76d57f1e8d940ceac9cdb3b6a779c0ee1
Anny Chacon <ac@plusteam.tech>
2021-08-19 10:54:14 -0400

    refactor: 🌻 Refactor names and class' names
    - Update translation file
    
    Refs #65cddt

--- addons/l10n_ve_plusteam/models/person_type.py
+++ addons/l10n_ve_plusteam/models/person_type.py
@@ -7,32 +7,31 @@ class PersonType(models.Model):
     _name = "person.type"
     _description = "Person Type"
 
-    code = fields.Char(string="Code")
+    code = fields.Char(string="Code", required=True)
     name = fields.Char(string="Description", translate=True, required=True)
-    tabulador = fields.One2many("tabulador.islr", "person_type_id", string="Tabulador")
+    islr_tab_ids = fields.One2many("islr.tab", "person_type_id", string="ISLR Tab")
 
 
 class TabuladorISLR(models.Model):
-    _name = "tabulador.islr"
-    _description = "Tabulador ISLR"
+    _name = "islr.tab"
+    _description = "ISLR Tab"
 
     concept_id = fields.Many2one('islr.concepts', string="ISRL Concepts", required=True)
     percentage_base = fields.Float(
         string="% Tax Base",
         default=0.0,
-        digits=2,
+        required=True,
         help=_("This percentage is applied to the amount of the service "
                "according to the concept or type of person to determine the tax base."))
     percentage_retention = fields.Float(
         string="% Retention",
         default=0.0,
-        digits=2,
+        required=True,
         help=_("It is the percentage that is applied to the tax base "
                "to calculate the withholding according to the concept and type of person."))
     subtracting = fields.Float(
         string="Subtracting",
         default=0.0,
-        digits=2,
         help=_("This is the amount that is calculated from the resident natural "
                "persons as an exempt amount of tax, that is to say that this amount is subtracted "
                "from the withholding calculated to obtain the withholding for resident natural persons."))
