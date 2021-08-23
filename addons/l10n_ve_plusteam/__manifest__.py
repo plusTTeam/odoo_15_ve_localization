29af5d1bbd33024e340d450b4fb1ef182a120fc7
Jeannette <gimezlug@gmail.com>
2021-08-23 17:27:45 -0400

    refactor: Se quito el strin del campo vat y agrego config en manifiest

--- addons/l10n_ve_plusteam/__manifest__.py
+++ addons/l10n_ve_plusteam/__manifest__.py
@@ -31,6 +31,7 @@
         'views/islr_concepts_views.xml',
         'views/person_type_views.xml',
         'views/tax_unit_views.xml',
+        'views/res_config_settings_views.xml',
         'views/menu_views.xml'
     ],
     # only loaded in demonstration mode
