b872b888039cd99c8174e61a542419c4c5652527
Jeannette <gimezlug@gmail.com>
2021-08-24 13:44:49 -0400

    refactor: Arreglos segun review

--- addons/l10n_ve_plusteam/models/account_move.py
+++ addons/l10n_ve_plusteam/models/account_move.py
@@ -9,8 +9,8 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     control_number = fields.Char(string="Control Number")
-    form_number = fields.Char(string="form number", help="Import form number C-80 o C-81", defaul="  ")
-    file_number = fields.Char(string="file number", help="Import file number", defaul="  ")
+    form_number = fields.Char(string="form number", help="Import form number C-80 or C-81")
+    file_number = fields.Char(string="file number", help="Import file number")
     import_doc = fields.Boolean(string="Import")
 
     @api.constrains('control_number')
