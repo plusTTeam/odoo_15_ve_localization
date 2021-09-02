1776c1af7458ef9e9454cbb7221d36909e330dec
Anny Chacon <ac@plusteam.tech>
2021-09-02 11:05:36 -0400

    refactor: 🌻 Remove unnecessary code
    Refs #CU-1f313wh

--- addons/l10n_ve_plusteam/models/resource_calendar.py
+++ addons/l10n_ve_plusteam/models/resource_calendar.py
@@ -8,17 +8,3 @@ class ResourceCalendar(models.Model):
 
     hours_per_day = fields.Float(string="Average Hour per Day",
                                  help=_("Average hours per day a resource is supposed to work with this calendar."))
-
-    @api.model
-    def remove_working_hours(self):
-        working_hour_40 = self.env.ref("resource.resource_calendar_std", raise_if_not_found=False)
-        if working_hour_40:
-            working_hour_40.update({
-                "name": _("Standard 40 hours/week")
-            })
-        working_hour_35 = self.env.ref("resource.resource_calendar_std_35h", raise_if_not_found=False)
-        if working_hour_35:
-            working_hour_35.unlink()
-        working_hour_38 = self.env.ref("resource.resource_calendar_std_38h", raise_if_not_found=False)
-        if working_hour_38:
-            working_hour_38.unlink()
