944cb3d4e6ed798ab26c6c6e56b3be8354d5873e
Anny Chacon <ac@plusteam.tech>
2021-09-02 12:33:11 -0400

    test: 🌻 Add unit tests
    Refs #CU-1f313wh

--- addons/l10n_ve_plusteam/tests/__init__.py
+++ addons/l10n_ve_plusteam/tests/__init__.py
@@ -4,4 +4,5 @@ from . import test_tax_unit
 from . import test_islr_concepts
 from . import test_account_move
 from . import test_resource_calendar_leaves
+from . import test_resource_calendar_attendance
 from . import test_res_lang
