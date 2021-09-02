3d372376d3ba1f25ad85c4ada01d40aa3a4896b7
Anny Chacon <ac@plusteam.tech>
2021-09-02 12:23:21 -0400

    test: 🌻 Add unit tests
    Refs #CU-1f313wh

---
+++ addons/l10n_ve_plusteam/tests/test_resource_calendar_attendance.py
@@ -0,0 +1,22 @@
+
+from odoo import fields
+from odoo.tests.common import TransactionCase
+
+
+class TestResourceCalendarAttendance(TransactionCase):
+
+    def setUp(self):
+        super(TestResourceCalendarAttendance, self).setUp()
+
+    def test_update_translations(self):
+        self.env["resource.calendar.attendance"].update_translations()
+        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Lunes por la mañana")])
+        self.assertEqual(attendance.name, "Lunes por la mañana", msg="Day not found")
+        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Martes por la mañana")])
+        self.assertEqual(attendance.name, "Martes por la mañana", msg="Day not found")
+        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Miércoles por la mañana")])
+        self.assertEqual(attendance.name, "Miércoles por la mañana", msg="Day not found")
+        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Jueves por la mañana")])
+        self.assertEqual(attendance.name, "Jueves por la mañana", msg="Day not found")
+        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Viernes por la mañana")])
+        self.assertEqual(attendance.name, "Viernes por la mañana", msg="Day not found")
