0a3636603b0c6ba3879a3a0e7b5e9b785e8af8b2
Anny Chacon <ac@plusteam.tech>
2021-09-02 12:50:57 -0400

    test: 🌻 Add unit tests
    Refs #CU-1f313wh

--- addons/l10n_ve_plusteam/tests/test_resource_calendar_attendance.py
+++ addons/l10n_ve_plusteam/tests/test_resource_calendar_attendance.py
@@ -10,13 +10,14 @@ class TestResourceCalendarAttendance(TransactionCase):
 
     def test_update_translations(self):
         self.env["resource.calendar.attendance"].update_translations()
-        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Lunes por la mañana")])
+        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Lunes por la mañana")], limit=1)
         self.assertEqual(attendance.name, "Lunes por la mañana", msg="Day not found")
-        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Martes por la mañana")])
+        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Martes por la mañana")], limit=1)
         self.assertEqual(attendance.name, "Martes por la mañana", msg="Day not found")
-        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Miércoles por la mañana")])
+        attendance = self.env["resource.calendar.attendance"].search(
+            [("name", "=", "Miércoles por la mañana")], limit=1)
         self.assertEqual(attendance.name, "Miércoles por la mañana", msg="Day not found")
-        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Jueves por la mañana")])
+        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Jueves por la mañana")], limit=1)
         self.assertEqual(attendance.name, "Jueves por la mañana", msg="Day not found")
-        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Viernes por la mañana")])
+        attendance = self.env["resource.calendar.attendance"].search([("name", "=", "Viernes por la mañana")], limit=1)
         self.assertEqual(attendance.name, "Viernes por la mañana", msg="Day not found")
