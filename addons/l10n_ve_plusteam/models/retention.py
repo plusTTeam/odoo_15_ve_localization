fa0527d1a6fb10cc8c844bffb5391660fa73e15a
Anny Chacon <ac@plusteam.tech>
2021-09-14 16:00:36 -0400

    feat: 🌻 Create journal entry for retention
    Refs #CU-6743tm

--- addons/l10n_ve_plusteam/models/retention.py
+++ addons/l10n_ve_plusteam/models/retention.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import logging
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
+_logger = logging.getLogger(__name__)
 
 
 class Retention(models.Model):
@@ -15,27 +17,19 @@ class Retention(models.Model):
         It could either be passed through the context using the 'default_journal_id' key containing its id,
         either be determined by the default type.
         """
-        move_type = self._context.get("default_move_type", "entry")
-        if move_type in self.get_sale_types(include_receipts=True):
-            journal_types = ["sale"]
-        elif move_type in self.get_purchase_types(include_receipts=True):
-            journal_types = ["purchase"]
-        else:
-            journal_types = self._context.get("default_move_journal_types", ["general"])
-
         if self._context.get("default_journal_id"):
             journal = self.env["account.journal"].browse(self._context["default_journal_id"])
-
-            if move_type != "entry" and journal.type not in journal_types:
-                raise ValidationError(_(
-                    "Cannot create an invoice of type %(move_type)s with a journal having %(journal_type)s as type.",
-                    move_type=move_type,
-                    journal_type=journal.type,
-                ))
         else:
-            journal = self._search_default_journal(journal_types)
+            journal = self._search_default_journal()
 
         return journal
+
+    @api.model
+    def _search_default_journal(self):
+        journal = self.env.ref("l10n_ve_plusteam.journal_withholding", raise_if_not_found=False)
+        if journal:
+            return journal
+        return self.env["account.journal"].search(["type", "=", "general"], limit=1)
 
     today = fields.Date.today()
     complete_name_with_code = fields.Char(
@@ -100,6 +94,8 @@ class Retention(models.Model):
     ref = fields.Char(string="Reference", related="invoice_number.ref")
     company_currency_id = fields.Many2one(related="invoice_number.company_currency_id", string="Company Currency",
                                           readonly=True, help="Utility field to express amount currency")
+    currency_id = fields.Many2one(related="invoice_number.currency_id", string="Currency", readonly=True,
+                                  help="Invoice currency")
     amount_tax = fields.Monetary(string="Amount tax", related="invoice_number.amount_tax",
                                  currency_field="company_currency_id")
     amount_untaxed = fields.Monetary(string="Amount tax", related="invoice_number.amount_untaxed",
@@ -184,10 +180,73 @@ class Retention(models.Model):
             invoice.write({
                 "retention_state": "with_retention_Both"
             })
-        journal_entry = self.env["account.move"].create({
-            "journal_id": self._get_default_journal()
-        })
-        return super(Retention, self).create(values)
+
+        retention = super(Retention, self).create(values)
+        retention.move_id.write(retention._update_move_id())
+        to_write['line_ids'] = [(0, 0, line_values) for line_values in retention._prepare_move_line_default_values()]
+        retention.move_id.write(to_write)
+        return retention
+
+    def _update_move_id(self):
+        self.ensure_one()
+        return {
+            "journal_id": self._get_default_journal().id,
+            "ref": _("%s withholding", self.retention_type),
+            "date": self.date,
+            "move_type": "entry",
+            "partner_id": self.partner_id.id,
+        }
+
+    def _prepare_move_line_default_values(self):
+        self.ensure_one()
+        default_line_name = self.code
+        if self.move_type in ('in_invoice', "in_refund"):
+            # Receive money.
+            counterpart_amount = self.amount_retention
+        elif self.move_type in ('out_invoice', "out_refund"):
+            # Send money.
+            counterpart_amount = -self.amount_retention
+        else:
+            counterpart_amount = 0.0
+        _logger.info(counterpart_amount)
+
+        balance = self.currency_id._convert(
+            counterpart_amount, self.company_id.currency_id, self.company_id, self.date)
+        _logger.info(balance)
+
+        counterpart_amount_currency = counterpart_amount
+        counterpart_account = self._get_counterpart_account()
+        _logger.info(self.destination_account_id)
+        line_ids = [{
+            'name': default_line_name,
+            'date_maturity': self.date,
+            'amount_currency': -counterpart_amount_currency,
+            'currency_id': self.currency_id.id,
+            'debit': balance < 0.0 and -balance or 0.0,
+            'credit': balance > 0.0 and balance or 0.0,
+            'partner_id': self.partner_id.id,
+            'account_id': self.destination_account_id.id
+        }, {
+            'name': default_line_name,
+            'date_maturity': self.date,
+            'amount_currency': -counterpart_amount_currency,
+            'currency_id': self.currency_id.id,
+            'debit': balance > 0.0 and balance or 0.0,
+            'credit': balance < 0.0 and -balance or 0.0,
+            'partner_id': self.partner_id.id,
+            'account_id': counterpart_account.id
+        }]
+        _logger.info(line_ids)
+
+        return line_ids
+
+    def _get_counterpart_account(self):
+        self.ensure_one()
+        lines = []
+        for line in self.invoice_number.line_ids:
+            if line.account_id.internal_type in ("receivable", "payable"):
+                lines.append(line)
+        return lines[0]
 
     @api.depends("ref", "code")
     def _compute_complete_name_with_code(self):
