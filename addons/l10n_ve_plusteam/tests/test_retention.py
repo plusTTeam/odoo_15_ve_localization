7a27dbff790244697e97be26433dd6853c4f71ce
Anny Chacon <ac@plusteam.tech>
2021-09-15 13:06:10 -0400

    test: 🌻 Add unit tests for cancel withholding
    Ref #CU-a73vd1

--- addons/l10n_ve_plusteam/tests/test_retention.py
+++ addons/l10n_ve_plusteam/tests/test_retention.py
@@ -26,15 +26,21 @@ class TestTaxUnit(TransactionCase):
             })]
         })
         self.invoice.write({"state": "posted"})
-
-    def test_create_retention(self):
-        retention = self.env["retention"].create({
+        self.retention = self.env["retention"].create({
             "invoice_number": self.invoice.id,
             "partner_id": self.partner.id,
             "move_type": self.invoice.move_type,
             "retention_type": RETENTION_TYPE_IVA,
             "vat_withholding_percentage": 75.0
         })
-        self.assertTrue(retention.destination_account_id is not False,
+
+    def test_create_retention(self):
+        self.assertTrue(self.retention.destination_account_id is not False,
                         msg="The destination account was not configured correctly")
-        self.assertTrue(len(retention.move_id.line_ids), msg="the account movements were not created")
+        self.assertTrue(len(self.retention.move_id.line_ids), msg="the account movements were not created")
+
+    def test_cancel_withholding(self):
+        self.retention.button_cancel()
+
+        self.assertEqual(self.retention.state, "cancel", msg="Withholding status was unchanged")
+        self.assertEqual(self.retention.move_id.state, "cancel", msg="Withholding status was unchanged")
