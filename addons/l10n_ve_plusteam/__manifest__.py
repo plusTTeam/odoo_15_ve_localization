503b0f66e915454cb66c4bf3c4fbb8d5699e3eab
Anny Chacon <ac@plusteam.tech>
2021-09-23 23:17:06 -0400

    feat: 🌻 Add IGTF Journal
    - Add igtf_journal_id in res_config_settings.py and res_company.py models
    - Add validation for igtf_account_id and igtf_journal_id
    - Create account.move when payment is created for IGTF
    - Make igtf field invisible when payment_type is different that outbound
    - Update translation file
    
    Refs #CU-69c6uc #CU-dzbta0

--- addons/l10n_ve_plusteam/__manifest__.py
+++ addons/l10n_ve_plusteam/__manifest__.py
@@ -52,6 +52,7 @@ Features:
         'data/resource_calendar_leaves_data.xml',
         'data/resource_calendar_attendance_data.xml',
         'data/resource_calendar_data.xml',
+        'data/account_journal_data.xml',
         'data/res_config_settings_data.xml',
         'data/res_country_data.xml',
         'views/res_partner_views.xml',
--- addons/l10n_ve_plusteam/models/account_payment.py
+++ addons/l10n_ve_plusteam/models/account_payment.py
@@ -1,4 +1,7 @@
-from odoo import models, fields, api
+import logging
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+_logger = logging.getLogger(__name__)
 
 
 class AccountPayment(models.Model):
@@ -7,6 +10,7 @@ class AccountPayment(models.Model):
     igtf = fields.Boolean(string="IGTF", default=False)
     igtf_amount = fields.Monetary(string="IGTF Amount", currency_field="currency_id",
                                   compute="_compute_igtf_amount", store=True)
+    igtf_move_id = fields.Many2one("account.move", string="IGTF Account Move")
 
     @api.depends("amount", "company_id", "igtf")
     def _compute_igtf_amount(self):
@@ -15,3 +19,58 @@ class AccountPayment(models.Model):
             if record.igtf is not False:
                 igtf_amount = record.amount * (record.company_id.igtf / 100)
             record.igtf_amount = igtf_amount
+
+    @api.model_create_multi
+    def create(self, values):
+        payments = super(AccountPayment, self).create(values)
+        for payment in payments:
+            if payment.igtf is not False and payment.payment_type == "outbound":
+                igtf_account = payment.company_id.igtf_account_id
+                if not igtf_account:
+                    raise ValidationError(
+                        _("There is not accounting account for the Tax on Big Financial Transactions (IGTF), "
+                          "please go to Settings and select an account to apply this tax")
+                    )
+                igtf_journal = payment.company_id.igtf_journal_id
+                if not igtf_journal:
+                    raise ValidationError(
+                        _("There is not journal for the Tax on Big Financial Transactions (IGTF), "
+                          "please go to Settings and select an journal to apply this tax")
+                    )
+                move_lines = payment.move_id.line_ids
+                counterpart_amount = -payment.igtf_amount
+                balance = payment.currency_id._convert(counterpart_amount, payment.company_id.currency_id,
+                                                       payment.company_id, payment.date)
+                lines = {
+                    "name": _("IGTF for payment %s", payment.name),
+                    "date_maturity": payment.date,
+                    "amount_currency": -counterpart_amount,
+                    "currency_id": payment.currency_id.id,
+                    "debit": 0.0,
+                    "credit": -balance,
+                    "partner_id": payment.partner_id.id,
+                    "account_id": move_lines[0].account_id.id
+                }, {
+                    "name": _("IGTF for payment %s", payment.name),
+                    "date_maturity": payment.date,
+                    "amount_currency": counterpart_amount,
+                    "currency_id": payment.currency_id.id,
+                    "debit": -balance,
+                    "credit": 0.0,
+                    "partner_id": payment.partner_id.id,
+                    "account_id": igtf_account.id
+                }
+                move = self.env["account.move"].create({
+                    "move_type": "entry",
+                    "partner_id": payment.partner_id.id,
+                    "journal_id": igtf_journal.id,
+                    "date": payment.date,
+                    "ref": payment.ref
+                })
+                move.write({"line_ids": [(0, 0, line) for line in lines]})
+                payment.write({"igtf_move_id": move.id})
+        return payments
+
+    def action_post(self):
+        super(AccountPayment, self).action_post()
+        self.igtf_move_id.write({"state": "posted"})
