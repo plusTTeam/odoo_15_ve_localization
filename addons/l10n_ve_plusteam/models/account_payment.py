25f1fdbbe2a8b57a953d874152eb2899ab7e73d0
Anny Chacon <ac@plusteam.tech>
2021-10-05 08:05:01 -0400

    feat: 🌻 Resolve comments in QA
    - Create IGTF in multi currency
    
    Refs #CU-69c6uc

--- addons/l10n_ve_plusteam/models/account_payment.py
+++ addons/l10n_ve_plusteam/models/account_payment.py
@@ -17,7 +17,8 @@ class AccountPayment(models.Model):
             igtf_amount = 0
             if record.igtf and record.hide_igtf is False and record.state == "draft":
                 igtf_amount = record.amount * (record.company_id.igtf / 100)
-            record.igtf_amount = igtf_amount
+            if record.state == "draft":
+                record.igtf_amount = igtf_amount
 
     @api.depends("journal_id.type", "company_id.igtf", "payment_type")
     def _compute_hide_igtf(self):
@@ -30,7 +31,7 @@ class AccountPayment(models.Model):
     def create(self, values):
         payments = super(AccountPayment, self).create(values)
         for payment in payments:
-            create_igtf_move = payment.igtf is not False and payment.igtf_amount != 0.0 and \
+            create_igtf_move = payment.igtf is not False and payment.igtf_amount > 0.0 and \
                                payment.payment_type == "outbound"
             if create_igtf_move:
                 igtf_account = payment.company_id.igtf_account_id
@@ -46,7 +47,7 @@ class AccountPayment(models.Model):
                           "please go to Settings and select an journal to apply this tax")
                     )
                 move_lines = payment.move_id.line_ids
-                counterpart_amount = -payment.igtf_amount
+                counterpart_amount = payment.igtf_amount
                 balance = payment.currency_id._convert(counterpart_amount, payment.company_id.currency_id,
                                                        payment.company_id, payment.date)
                 lines = {
@@ -55,7 +56,7 @@ class AccountPayment(models.Model):
                     "amount_currency": -counterpart_amount,
                     "currency_id": payment.currency_id.id,
                     "debit": 0.0,
-                    "credit": abs(balance),
+                    "credit": balance,
                     "partner_id": payment.partner_id.id,
                     "account_id": move_lines[0].account_id.id
                 }, {
@@ -63,7 +64,59 @@ class AccountPayment(models.Model):
                     "date_maturity": payment.date,
                     "amount_currency": counterpart_amount,
                     "currency_id": payment.currency_id.id,
-                    "debit": abs(balance),
+                    "debit": balance,
+                    "credit": 0.0,
+                    "partner_id": payment.partner_id.id,
+                    "account_id": igtf_account.id
+                }
+                move = self.env["account.move"].create({
+                    "move_type": "entry",
+                    "partner_id": payment.partner_id.id,
+                    "journal_id": igtf_journal.id,
+                    "date": payment.date,
+                    "ref": payment.ref
+                })
+                move.write({"line_ids": [(0, 0, line) for line in lines]})
+                payment.write({"igtf_move_id": move.id})
+        return payments
+
+    def write(self, values):
+        payments = super(AccountPayment, self).write(values)
+        if values.get("currency_id", False):
+            create_igtf_move = payment.igtf is not False and payment.igtf_amount > 0.0 and \
+                               payment.payment_type == "outbound"
+            if create_igtf_move:
+                igtf_account = payment.company_id.igtf_account_id
+                if not igtf_account:
+                    raise ValidationError(
+                        _("There is not accounting account for the Tax on Big Financial Transactions (IGTF), "
+                          "please go to Settings and select an account to apply this tax")
+                    )
+                igtf_journal = payment.company_id.igtf_journal_id
+                if not igtf_journal:
+                    raise ValidationError(
+                        _("There is not journal for the Tax on Big Financial Transactions (IGTF), "
+                          "please go to Settings and select an journal to apply this tax")
+                    )
+                move_lines = payment.move_id.line_ids
+                counterpart_amount = payment.igtf_amount
+                balance = payment.currency_id._convert(counterpart_amount, payment.company_id.currency_id,
+                                                       payment.company_id, payment.date)
+                lines = {
+                    "name": _("IGTF for payment %s", payment.name),
+                    "date_maturity": payment.date,
+                    "amount_currency": -counterpart_amount,
+                    "currency_id": payment.currency_id.id,
+                    "debit": 0.0,
+                    "credit": balance,
+                    "partner_id": payment.partner_id.id,
+                    "account_id": move_lines[0].account_id.id
+                }, {
+                    "name": _("IGTF for payment %s", payment.name),
+                    "date_maturity": payment.date,
+                    "amount_currency": counterpart_amount,
+                    "currency_id": payment.currency_id.id,
+                    "debit": balance,
                     "credit": 0.0,
                     "partner_id": payment.partner_id.id,
                     "account_id": igtf_account.id
