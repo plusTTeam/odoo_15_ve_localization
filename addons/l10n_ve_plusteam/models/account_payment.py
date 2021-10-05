9e19fb4e19767cb38c5a73e4ada5547a8f6221db
Anny Chacon <anny.chacon@unet.edu.ve>
2021-10-05 16:08:39 -0400

    feat: 🌻 Update igtf values in write method
    Refs #CU-69c6uc

--- addons/l10n_ve_plusteam/models/account_payment.py
+++ addons/l10n_ve_plusteam/models/account_payment.py
@@ -3,7 +3,7 @@ from odoo.exceptions import ValidationError
 
 
 class AccountPayment(models.Model):
-    _inherit = 'account.payment'
+    _inherit = "account.payment"
 
     igtf = fields.Boolean(string="IGTF", default=False)
     igtf_amount = fields.Monetary(string="IGTF Amount", currency_field="currency_id",
@@ -46,29 +46,6 @@ class AccountPayment(models.Model):
                         _("There is not journal for the Tax on Big Financial Transactions (IGTF), "
                           "please go to Settings and select an journal to apply this tax")
                     )
-                move_lines = payment.move_id.line_ids
-                counterpart_amount = payment.igtf_amount
-                balance = payment.currency_id._convert(counterpart_amount, payment.company_id.currency_id,
-                                                       payment.company_id, payment.date)
-                lines = {
-                    "name": _("IGTF for payment %s", payment.name),
-                    "date_maturity": payment.date,
-                    "amount_currency": -counterpart_amount,
-                    "currency_id": payment.currency_id.id,
-                    "debit": 0.0,
-                    "credit": balance,
-                    "partner_id": payment.partner_id.id,
-                    "account_id": move_lines[0].account_id.id
-                }, {
-                    "name": _("IGTF for payment %s", payment.name),
-                    "date_maturity": payment.date,
-                    "amount_currency": counterpart_amount,
-                    "currency_id": payment.currency_id.id,
-                    "debit": balance,
-                    "credit": 0.0,
-                    "partner_id": payment.partner_id.id,
-                    "account_id": igtf_account.id
-                }
                 move = self.env["account.move"].create({
                     "move_type": "entry",
                     "partner_id": payment.partner_id.id,
@@ -76,61 +53,52 @@ class AccountPayment(models.Model):
                     "date": payment.date,
                     "ref": payment.ref
                 })
-                move.write({"line_ids": [(0, 0, line) for line in lines]})
+                move.write({"line_ids": [(0, 0, line) for line in self.get_lines()]})
                 payment.write({"igtf_move_id": move.id})
         return payments
 
     def write(self, values):
-        payments = super(AccountPayment, self).write(values)
+        payment = super(AccountPayment, self).write(values)
         if values.get("currency_id", False):
-            create_igtf_move = payment.igtf is not False and payment.igtf_amount > 0.0 and \
-                               payment.payment_type == "outbound"
-            if create_igtf_move:
-                igtf_account = payment.company_id.igtf_account_id
-                if not igtf_account:
-                    raise ValidationError(
-                        _("There is not accounting account for the Tax on Big Financial Transactions (IGTF), "
-                          "please go to Settings and select an account to apply this tax")
-                    )
-                igtf_journal = payment.company_id.igtf_journal_id
-                if not igtf_journal:
-                    raise ValidationError(
-                        _("There is not journal for the Tax on Big Financial Transactions (IGTF), "
-                          "please go to Settings and select an journal to apply this tax")
-                    )
-                move_lines = payment.move_id.line_ids
-                counterpart_amount = payment.igtf_amount
-                balance = payment.currency_id._convert(counterpart_amount, payment.company_id.currency_id,
-                                                       payment.company_id, payment.date)
-                lines = {
-                    "name": _("IGTF for payment %s", payment.name),
-                    "date_maturity": payment.date,
-                    "amount_currency": -counterpart_amount,
-                    "currency_id": payment.currency_id.id,
-                    "debit": 0.0,
-                    "credit": balance,
-                    "partner_id": payment.partner_id.id,
-                    "account_id": move_lines[0].account_id.id
-                }, {
-                    "name": _("IGTF for payment %s", payment.name),
-                    "date_maturity": payment.date,
-                    "amount_currency": counterpart_amount,
-                    "currency_id": payment.currency_id.id,
-                    "debit": balance,
-                    "credit": 0.0,
-                    "partner_id": payment.partner_id.id,
-                    "account_id": igtf_account.id
-                }
-                move = self.env["account.move"].create({
-                    "move_type": "entry",
-                    "partner_id": payment.partner_id.id,
-                    "journal_id": igtf_journal.id,
-                    "date": payment.date,
-                    "ref": payment.ref
-                })
-                move.write({"line_ids": [(0, 0, line) for line in lines]})
-                payment.write({"igtf_move_id": move.id})
-        return payments
+            payment.igtf_move_id.write({"currency_id": values["currency_id"]})
+        if values.get("date", False):
+            payment.igtf_move_id.write({"date": values["date"]})
+        if values.get("amount", False) or values.get("date", False) or values.get("partner", False) or values.get("currency_id", False) or values.get("company", False):
+            payment.update_igtf_lines()
+        if values.get("payment_type", False) == "inbound":
+            payment.igtf_move_id.unlink()
+
+    def update_igtf_lines(self):
+        self.ensure_one()
+        self.igtf_move_id.write({"line_ids": [(6, 0, self.igtf_move_id.line_ids)]})
+        self.igtf_move_id.write({"line_ids": [(0, 0, line) for line in self.get_lines()]})
+
+    def get_lines(self):
+        self.ensure_one()
+        payment = self
+        move_lines = payment.move_id.line_ids
+        counterpart_amount = payment.igtf_amount
+        balance = payment.currency_id._convert(counterpart_amount, payment.company_id.currency_id,
+                                               payment.company_id, payment.date)
+        return {
+            "name": _("IGTF for payment %s", payment.name),
+            "date_maturity": payment.date,
+            "amount_currency": -counterpart_amount,
+            "currency_id": payment.currency_id.id,
+            "debit": 0.0,
+            "credit": balance,
+            "partner_id": payment.partner_id.id,
+            "account_id": move_lines[0].account_id.id
+        }, {
+            "name": _("IGTF for payment %s", payment.name),
+            "date_maturity": payment.date,
+            "amount_currency": counterpart_amount,
+            "currency_id": payment.currency_id.id,
+            "debit": balance,
+            "credit": 0.0,
+            "partner_id": payment.partner_id.id,
+            "account_id": igtf_account.id
+        }
 
     def action_post(self):
         super(AccountPayment, self).action_post()
