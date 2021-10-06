086ebf68c215956510c7f9bfdbaa6d14bff73479
Jeannette <gimezlug@gmail.com>
2021-10-06 19:38:08 -0400

    refactor: cambio atributos numero de control y documento

--- addons/l10n_ve_plusteam/models/account_move.py
+++ addons/l10n_ve_plusteam/models/account_move.py
@@ -8,11 +8,12 @@ from odoo.exceptions import ValidationError
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    control_number = fields.Char(string="Control Number")
+    control_number = fields.Char(string="Control Number", states={'draft': [('readonly', False)]})
     form_number = fields.Char(string="form number", help="Import form number C-80 or C-81")
     file_number = fields.Char(string="file number", help="Import file number")
     import_doc = fields.Boolean(string="Import")
-    invoice_number = fields.Char(string="Invoice Number", help="Is defined when it is a supplier invoice")
+    invoice_number = fields.Char(string="Invoice Number", help="Is defined when it is a supplier invoice",
+                                 states={'draft': [('readonly', False)]})
     fiscal_printing_serial = fields.Char(
         string="Fiscal Printing Serial",
         help="Defined when the invoice is printed to the customer"
